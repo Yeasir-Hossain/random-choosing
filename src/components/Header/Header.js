1f49feced4621dc842bb18289c34da759bb3b74a
Yeasir-Hossain <yeasir06@gmail.com>
2022-03-26 03:25:28 +0600

    Initialized

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+      <div className='header'>
+        <h1>CHOOSE YOUR HEADPHONE</h1>
+        <h3>Choose any 4 headphones</h3>
+      </div>
+    );
+};
+
+export default Header;
